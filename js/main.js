f3f6ed65b24b5015d585ca2034284c88c445ec7e
SirPm <odumosuoluwapm@gmail.com>
2020-08-21 01:34:40 +0100

    Initial Upload

---
+++ js/main.js
@@ -0,0 +1,24 @@
+navSlide = () => {
+    const nav = document.querySelector('.nav-links');
+    const burger = document.querySelector('#ham');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', () => {
+        // Toggle nav
+        nav.classList.toggle('nav-active');
+
+        // Animate links
+        navLinks.forEach( (link, index) => {
+            if(link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.5}s`;
+            }
+        });
+
+        // Animate the burger button
+        burger.classList.toggle('toggleBurger');
+    });
+}
+
+navSlide();
